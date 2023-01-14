0c55fd38b6df6a86a03e44ab8da0c52546039a9b
VadimKorobec <_s_e_t__@ukr.net>
2023-01-14 17:10:53 +0200

    Update FeedbackOptions.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,16 +1,16 @@
-import import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-export const FeedbackOptions = ({options,onBtnClick}) => (
-    <div>
-        {options.map(option => (
-            <button type='button' key={option} onClick={() => onBtnClick(option)}>
-                {option}
-            </button>
-        ))}
-    </div>
-)
+export const FeedbackOptions = ({ options, onBtnClick }) => (
+  <div>
+    {options.map(option => (
+      <button type="button" key={option} onClick={() => onBtnClick(option)}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.array.isRequired,
-    onBtnClick
-}
+  options: PropTypes.array.isRequired,
+  onBtnClick: PropTypes.func.isRequired,
+};
