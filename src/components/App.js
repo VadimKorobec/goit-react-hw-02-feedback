92ff2b87ad69f6b05943415743191c593d0924ff
VadimKorobec <_s_e_t__@ukr.net>
2023-01-14 16:53:04 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,9 @@ export class App extends Component {
       <div>
         <h2>Please leave feedbeck</h2>
         <div>
-          <button type="button">Good</button>
+          <button type="button" onClick={() => onBtnClick(option)}>
+            Good
+          </button>
           <button type="button">Neutral</button>
           <button type="button">Bad</button>
         </div>
