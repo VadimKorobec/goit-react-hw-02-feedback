83619a87e804ad720a61d44cf3db9de41b69dbd8
VadimKorobec <_s_e_t__@ukr.net>
2023-01-14 17:27:48 +0200

    Update Section.js

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import { Container } from './Section.styled';
 
 export const Section = ({ title, children }) => (
-  <div>
+  <Container>
     <h2>{title}</h2>
     {children}
-  </div>
+  </Container>
 );
 
 Section.propTypes = {
