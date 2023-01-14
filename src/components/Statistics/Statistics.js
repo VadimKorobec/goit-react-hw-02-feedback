2a037a232be6f9f66667a40090db5c68a67d706c
VadimKorobec <_s_e_t__@ukr.net>
2023-01-14 14:11:55 +0200

    Create Statistics.js

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = () => (
+    <div>
+        <ul>
+            <li>Good:{good }</li>
+            <li>Neutral:{neutral }</li>
+            <li>Bad:{ bad}</li>
+            <li>Total:{total }</li>
+            <li>Positive:</li>
+
+        </ul>
+        <div/>
+)
+    
+    
