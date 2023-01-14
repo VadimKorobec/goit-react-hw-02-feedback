bf6f4c1909c5a3f6df9c8e2e5c07c1f344dee7f9
VadimKorobec <_s_e_t__@ukr.net>
2023-01-14 16:10:53 +0200

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 
-export const Statistics = () => (
-    <div>
-        <ul>
-            <li>Good:{good }</li>
-            <li>Neutral:{neutral }</li>
-            <li>Bad:{ bad}</li>
-            <li>Total:{total }</li>
-            <li>Positive:</li>
-
-        </ul>
-        <div/>
-)
-    
-    
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => (
+  <>
+    <ul>
+      <li>Good:{good}</li>
+      <li>Neutral:{neutral}</li>
+      <li>Bad:{bad}</li>
+      <li>Total:{total}</li>
+      <li>Positive feedback:{positivePercentage}%</li>
+    </ul>
+  </>
+);
